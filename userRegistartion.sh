e12b102e2fb681a0b38baea212408489701cfd0a
AniketGujar <gujaraniket7875@gmail.com>
2020-11-05 18:01:50 +0530

    Added: User Email Validation

--- userRegistartion.sh
+++ userRegistartion.sh
@@ -2,20 +2,29 @@
 
 read -p "Enter First Name " fName
 read -p "Enter Second Name " lName
+read -p "Enter Email ID " email
 
 fnp="^[A-Z]{1}[a-zA-Z]{2,10}"
 lnp="^[A-Z]{1}[a-zA-Z]{2,10}"
+ep="^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)*[@][a-Z]+[.][a-z]+([.][a-z]+)*$"
 
 if [[ $fName =~ $fnp ]]
 then
-	echo "Match"
+	echo "Valid First Name"
 else
-	echo "Error"
+	echo "Invalid First Name"
 fi
 
 if [[ $lName =~ $lnp ]]
 then
-        echo "Second Name Correct"
+        echo "Last Name Correct"
 else
-        echo "Error Last Name"
+        echo "Invalid Last Name"
+fi
+
+if [[ $email =~ $ep ]]
+then
+	echo "Valid Email Id"
+else
+	echo "Incorrect Email Id"
 fi
