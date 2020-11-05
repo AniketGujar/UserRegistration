b4a2858a0c11d5cecf92341430e8f1d8c6d10793
AniketGujar <gujaraniket7875@gmail.com>
2020-11-05 17:30:42 +0530

    Added: User Last Name Validation

--- userRegistartion.sh
+++ userRegistartion.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-read fName
+read -p "Enter First Name " fName
+read -p "Enter Second Name " lName
+
 fnp="^[A-Z]{1}[a-zA-Z]{2,10}"
+lnp="^[A-Z]{1}[a-zA-Z]{2,10}"
+
 if [[ $fName =~ $fnp ]]
 then
 	echo "Match"
 else
 	echo "Error"
+fi
+
+if [[ $lName =~ $lnp ]]
+then
+        echo "Second Name Correct"
+else
+        echo "Error Last Name"
 fi
